0021dfc0ffba1902d36b72e17e25af925e551abd
veto <veto@body.body-piercing.com>
2026-02-16 13:34:41 +0700

    up: public/#main.js# public/index.html public/main.js public/node_module/log2textarea.js

--- public/main.js
+++ public/main.js
@@ -1,17 +1,19 @@
+var log = new Log2textarea("logger");
+
 window.onload = function () {
   const $button1 = document.querySelector("#btn_1");
   const $button2 = document.querySelector("#btn_2");
   const $button3 = document.querySelector("#btn_3");
 
-  $button1.addEventListener("click", () => {
-    console.log("button 3 clicked");
+  $button1.addEventListener("click", (e) => {
+    log.info("...click button: " + e.target.id);
   });
 
-  $button2.addEventListener("click", () => {
-    console.log("button 2 clicked");
+  $button2.addEventListener("click", (e) => {
+    log.info("...click button: " + e.target.id);
   });
 
-  $button3.addEventListener("click", () => {
-    console.log("button 3 clicked");
+  $button3.addEventListener("click", (e) => {
+    log.info("...click button: " + e.target.id);
   });
 };

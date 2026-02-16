3224a7226dcf461d1b3431706d48639c0bdbc61c
veto <veto@lab.local>
2026-02-16 20:23:38 +0700

    up: pages/umd.html pages/umd.js

---
+++ pages/umd.js
@@ -0,0 +1,20 @@
+//import { Log2textarea } from "./dist/log2textarea.umd.js";
+//var log = new Log2textarea("logger");
+
+window.onload = function () {
+  const $button1 = document.querySelector("#btn_1");
+  const $button2 = document.querySelector("#btn_2");
+  const $button3 = document.querySelector("#btn_3");
+
+  $button1.addEventListener("click", (e) => {
+    log.info("...click button: " + e.target.id);
+  });
+
+  $button2.addEventListener("click", (e) => {
+    log.info("...click button: " + e.target.id);
+  });
+
+  $button3.addEventListener("click", (e) => {
+    log.info("...click button: " + e.target.id);
+  });
+};
